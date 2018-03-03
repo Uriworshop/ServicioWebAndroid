3bdeba0fc742cba3591faa7edc0b52e1123b04e2
Uriworshop <uri_el_12@hotmail.com>
2018-03-02 20:28:43 -0600

    Inserción inicial en el repositorio del módulo Practica4ParcialUYS

---
+++ Practica4ParcialUYS/Datos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Practica4ParcialUYS
+{
+	[Activity(Label = "Datos")]
+	public class Datos : Activity
+	{
+		string peli1 = "Donnie";
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Rese);
+			TextView Titulo = FindViewById<TextView>(Resource.Id.txtTitulo);
+			TextView Clasificacion = FindViewById<TextView>(Resource.Id.txtClasifica);
+			TextView Genero = FindViewById<TextView>(Resource.Id.txtGenero);
+			TextView Duracion = FindViewById<TextView>(Resource.Id.txtDuracion);
+			TextView Director = FindViewById<TextView>(Resource.Id.txtDirector);
+			TextView Actores = FindViewById<TextView>(Resource.Id.txtActores);
+			TextView Sinopsis = FindViewById<TextView>(Resource.Id.txtRese);
+			TextView Fecha = FindViewById<TextView>(Resource.Id.txtFecha);
+			ImageView LaImagen = FindViewById<ImageView>(Resource.Id.ImgBase);
+
+			TextView Gen = FindViewById<TextView>(Resource.Id.txtGenero);
+			Gen.MovementMethod = new Android.Text.Method.ScrollingMovementMethod();
+			TextView Res = FindViewById<TextView>(Resource.Id.txtRese);
+			Res.MovementMethod = new Android.Text.Method.ScrollingMovementMethod();
+			TextView Act = FindViewById<TextView>(Resource.Id.txtActores);
+			Act.MovementMethod = new Android.Text.Method.ScrollingMovementMethod();
+
+
+			try
+			{
+				Titulo.Text = Intent.GetStringExtra("Titu");
+				Clasificacion.Text = Intent.GetStringExtra("Clasi");
+				Genero.Text = Intent.GetStringExtra("Gene");
+				Duracion.Text = Intent.GetStringExtra("Dura");
+				Director.Text = Intent.GetStringExtra("Dire");
+				Actores.Text = Intent.GetStringExtra("Acto");
+				Sinopsis.Text = Intent.GetStringExtra("Sino");
+				Fecha.Text = Intent.GetStringExtra("Fe");
+				if (Titulo.Text == "Donnie Darko")
+					LaImagen.SetImageResource(Resource.Drawable.Donnie);
+				if (Titulo.Text == "El efecto mariposa")
+					LaImagen.SetImageResource(Resource.Drawable.Mariposa);
+				if (Titulo.Text == "El exorcista")
+					LaImagen.SetImageResource(Resource.Drawable.Diablo);
+				if (Titulo.Text == "El increible castillo vagabundo")
+					LaImagen.SetImageResource(Resource.Drawable.Castillo);
+				if (Titulo.Text == "Intouchables")
+					LaImagen.SetImageResource(Resource.Drawable.Intocables);
+				if (Titulo.Text == "La naranja mecanica")
+					LaImagen.SetImageResource(Resource.Drawable.Naranja);
+				if (Titulo.Text == "Las ventajas de ser invisible")
+					LaImagen.SetImageResource(Resource.Drawable.Ventajas);
+				if (Titulo.Text == "Requiem por un sueno")
+					LaImagen.SetImageResource(Resource.Drawable.Requim);
+				if (Titulo.Text == "Straight Outta Compton" || Titulo.Text == "Straight outta compton ")
+					LaImagen.SetImageResource(Resource.Drawable.Outta);
+				if (Titulo.Text == "Wolverine inmortal" || Titulo.Text == "Wolverine Inmortal")
+					LaImagen.SetImageResource(Resource.Drawable.Wolver);
+			}
+			catch (Exception)
+			{
+				Toast.MakeText(this, "Error Al Cargar Datos", ToastLength.Long).Show();
+			}
+
+		}
+	}
+}
---
+++ Practica4ParcialUYS/MainActivity.cs
@@ -0,0 +1,542 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Data;
+using Android.Views.Animations;
+using System.Threading.Tasks;
+using Android.Content;
+using System;
+
+namespace Practica4ParcialUYS
+{
+	[Activity(Label = "Practica4ParcialUYS", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		string Titu, Clasi, Gene, Dura, Dire, Acto, Sino, Fe;
+		Animation fade_in, fade_put;
+		ViewFlipper Imagen;
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+
+			ImageButton Boton1 = FindViewById<ImageButton>(Resource.Id.imageButton1);
+			ImageButton Boton2 = FindViewById<ImageButton>(Resource.Id.imageButton2);
+			ImageButton Boton3 = FindViewById<ImageButton>(Resource.Id.imageButton3);
+			ImageButton Boton4 = FindViewById<ImageButton>(Resource.Id.imageButton4);
+			ImageButton Boton5 = FindViewById<ImageButton>(Resource.Id.imageButton5);
+			ImageButton Boton6 = FindViewById<ImageButton>(Resource.Id.imageButton6);
+			ImageButton Boton7 = FindViewById<ImageButton>(Resource.Id.imageButton7);
+			ImageButton Boton8 = FindViewById<ImageButton>(Resource.Id.imageButton8);
+			ImageButton Boton9 = FindViewById<ImageButton>(Resource.Id.imageButton9);
+			ImageButton Boton10 = FindViewById<ImageButton>(Resource.Id.imageButton10);
+			ImageButton Boton11 = FindViewById<ImageButton>(Resource.Id.imageButton11);
+			Imagen = FindViewById<ViewFlipper>(Resource.Id.ImagenesCar);
+
+			fade_in = AnimationUtils.LoadAnimation(this, Resource.Animation.Fade_in);
+			fade_put = AnimationUtils.LoadAnimation(this, Resource.Animation.Fade_put);
+
+			Imagen.SetInAnimation(ApplicationContext, Resource.Animation.Fade_in);
+			Imagen.SetOutAnimation(ApplicationContext, Resource.Animation.Fade_put);
+			Imagen.SetFlipInterval(5000);
+			Imagen.StartFlipping();
+
+			Boton1.Click += delegate{
+				try
+				{   
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("Donnie Darko");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("Donnie Darko");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+
+				}
+			};
+
+			Boton2.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("El efecto mariposa");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("El efecto mariposa");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton3.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("El exorcista");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("El exorcista");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton4.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("El increible castillo vagabundo");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("El increible castillo vagabundo");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton5.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("Intouchables");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("Intouchables");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton6.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("La naranja mecanica");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("La naranja mecanica");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton7.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("Las ventajas de ser invisible");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("Las ventajas de ser invisible");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton8.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("Requiem por un sueno");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("Requiem por un sueno");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton9.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("Straight Outta Compton");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("Straight outta compton");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton10.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("Wolverine inmortal");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					try
+					{
+						Practica4ParcialUYS.ServiCineSql.Service Ser = new Practica4ParcialUYS.ServiCineSql.Service();
+						DataSet Con = new DataSet();
+						Con = Ser.Consultar("Wolverine Inmortal");
+						DataRow Renglon;
+						Renglon = Con.Tables["Informacion"].Rows[0];
+						Titu = Renglon["Nombre"].ToString();
+						Gene = Renglon["Genero"].ToString();
+						Clasi = Renglon["Clasificacion"].ToString();
+						Dura = Renglon["Duracion"].ToString();
+						Dire = Renglon["Director"].ToString();
+						Acto = Renglon["Actores"].ToString();
+						Sino = Renglon["Sinopsis"].ToString();
+						Fe = Renglon["Fecha_Estreno"].ToString();
+						Cargar();
+
+					}
+					catch (Exception)
+					{
+						Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+					}
+				}
+			};
+			Boton11.Click += delegate
+			{
+				try
+				{
+					Practica4ParcialUYS.SitioCine.ServiCine Ser = new Practica4ParcialUYS.SitioCine.ServiCine();
+					DataSet Con = new DataSet();
+					Con = Ser.BuscarPeli("Comunidad");
+					DataRow Renglon;
+					Renglon = Con.Tables["Informacion"].Rows[0];
+					Titu = Renglon["Nombre"].ToString();
+					Gene = Renglon["Genero"].ToString();
+					Clasi = Renglon["Clasificacion"].ToString();
+					Dura = Renglon["Duracion"].ToString();
+					Dire = Renglon["Director"].ToString();
+					Acto = Renglon["Actores"].ToString();
+					Sino = Renglon["Sinopsis"].ToString();
+					Fe = Renglon["Fecha"].ToString();
+					Cargar();
+				}
+				catch (System.Exception)
+				{
+					Toast.MakeText(this, "Erro Al Busca La Peli", ToastLength.Long).Show();
+				}
+			};
+		}
+
+		void Ser_BuscarPeliCompleted(object sender, SitioCine.BuscarPeliCompletedEventArgs e)
+		{
+			
+		}
+
+		public void Cargar()
+		{
+			Intent Datos = new Intent(this, typeof(Datos));
+			Datos.PutExtra("Titu", Titu);
+			Datos.PutExtra("Gene", Gene);
+			Datos.PutExtra("Clasi", Clasi);
+			Datos.PutExtra("Dura", Dura);
+			Datos.PutExtra("Dire", Dire);
+			Datos.PutExtra("Acto", Acto);
+			Datos.PutExtra("Sino", Sino);
+			Datos.PutExtra("Fe", Fe);
+			StartActivity(Datos);
+		}
+	}
+}
+
